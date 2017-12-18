917151a4557c4fe135379e2661aece2a8e0a5868
mousterian <rivladi@gmail.com>
2017-12-18 14:50:26 -0800

    - fetches all root folders on the load of the page - refetches all root folders once a new folder's been created - basic html table for display of fetched data

--- src/app/components/file-list/file-list.ts
+++ src/app/components/file-list/file-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FileService } from '../../services/file.svc';
 import { FolderService } from '../../services/folder.svc';
 
@@ -7,25 +7,51 @@ import { FolderService } from '../../services/folder.svc';
 	templateUrl: './file-list.html',
 })
 
-export class FileListComponent { 
+export class FileListComponent implements OnInit{ 
 	
 	folderName: string = '';
+	foldersAtRootArray: any[] = [];
 
 	constructor(
 		private fileSvc: FileService,
 		private folderSvc: FolderService) {
-
 	}
+
+	ngOnInit() {
+		this.getRootData();
+	}
+
+	// a button click to create a new folder.
+	// sends a payload with a 'name' parameter
+	// for now, triggers getRootData() method and refetches all data
+	// TODO: update data in local cache?
 
 	createNewFolder() {
 		let payload = {
 			name: this.folderName
 		}
-		this.folderSvc.create(payload).then((response) => {
-			console.log(response)
+		this.folderSvc.create(payload)
+		.then((response) => {
+			// clear the folder name
+			this.folderName = null;
+			return new Promise((resolve, reject) => {
+				resolve(response);
+			})
+		})
+		.then((response) => {
+			this.getRootData();
 		})
 		.catch(err => {
-			console.log(err)
+			// console.log(err)
+		})
+	}
+
+	getRootData() {
+		this.fileSvc.getAllFiles().then((response) => {
+			this.foldersAtRootArray = response;
+		})
+		.catch(err => {
+			// console.log(err);
 		})
 	}
 }
